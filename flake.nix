55bba2a8abab2198e0a65ecab99a6b2190d05783
Florian RICHER <florian.richer@unova.fr>
2023-08-20 00:11:19 +0200

    Nix OS flake update

--- flake.nix
+++ flake.nix
@@ -9,13 +9,21 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      "nixos-test" = nixpkgs.lib.nixosSystem {
+      # nixos => hostname
+      nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
         modules = [
           ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.florian = import ./home.nix;
+          }
         ];
       };
     };
