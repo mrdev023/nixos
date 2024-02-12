5c1145e49cd5e751ed9d6977b2d4d9818fad638d
Florian RICHER <florian.richer@protonmail.com>
2024-02-12 14:11:22 +0100

    Add nvidia

--- hosts/perso-laptop/default.nix
+++ hosts/perso-laptop/default.nix
@@ -9,6 +9,7 @@
     [
       ../../modules/system.nix
       ../../modules/network.nix
+      ../../modules/nvidia.nix
       ../../modules/plasma.nix
       ../../modules/keymaps/fr.nix
       ../../modules/pipewire.nix
@@ -22,4 +23,16 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-laptop-perso"; # Define your hostname.
+
+  hardware.nvidia.prime = {
+    offload = {
+			enable = true;
+			enableOffloadCmd = true;
+		};
+		# Make sure to use the correct Bus ID values for your system!
+    # information bus: pci@0000:00:02.0
+		intelBusId = "PCI:0:2:0";
+    # information bus: pci@0000:01:00.0
+		nvidiaBusId = "PCI:1:0:0";
+	};
 }
