1442b171901baa20766a0b96ad6d113cac980811
Florian RICHER <florian.richer@protonmail.com>
2024-02-17 11:54:36 +0100

    Add basics openssh server to help develop in my laptop

--- hosts/perso-laptop/default.nix
+++ hosts/perso-laptop/default.nix
@@ -13,6 +13,7 @@
       ../../modules/plasma.nix
       ../../modules/keymaps/fr.nix
       ../../modules/pipewire.nix
+      ../../modules/openssh.nix
 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
---
+++ modules/openssh.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+services.openssh = {
+  enable = true;
+  passwordAuthentication = true;
+  listenAddresses = [
+    {
+      addr = "192.168.1.0/24";
+      port = "22";
+    }
+  ]
+};
