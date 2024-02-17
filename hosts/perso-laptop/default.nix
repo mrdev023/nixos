9933e82a9f6e6f39be8702284262f149e9d5bc74
Florian RICHER <florian.richer@protonmail.com>
2024-02-17 12:50:46 +0100

    [openssh][perso-laptop] Re-enable

--- hosts/perso-laptop/default.nix
+++ hosts/perso-laptop/default.nix
@@ -13,6 +13,7 @@
       ../../modules/plasma.nix
       ../../modules/keymaps/fr.nix
       ../../modules/pipewire.nix
+      ../../modules/openssh.nix
       ../../modules/openvscode-server.nix
 
       # Include the results of the hardware scan.
