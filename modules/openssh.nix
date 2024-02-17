b8f904b54355f0257050c18ed198353960b2d835
Florian RICHER <florian.richer@protonmail.com>
2024-02-17 11:56:20 +0100

    Try fix

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -1,12 +1,14 @@
 { config, pkgs, ... }:
 
-services.openssh = {
-  enable = true;
-  passwordAuthentication = true;
-  listenAddresses = [
-    {
-      addr = "192.168.1.0/24";
-      port = "22";
-    }
-  ]
-};
+{
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = true;
+    listenAddresses = [
+      {
+        addr = "192.168.1.0/24";
+        port = "22";
+      }
+    ];
+  };
+}
