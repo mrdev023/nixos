22fc39a6523cb38a76dcdad8d40cdc2c2783a9a2
Florian RICHER <florian.richer@protonmail.com>
2024-04-12 19:06:48 +0200

    Update OS

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -4,8 +4,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
+  services.displayManager = {
+    sddm.enable = true;
+    defaultSession = "plasma";
+  };
   services.desktopManager.plasma6.enable = true;
 
   programs.kdeconnect.enable = true;
