900fd073996b18324ea30f1b1b3b3de3fef988b4
Florian RICHER <florian.richer@unova.fr>
2024-05-05 18:58:12 +0200

    Change shell to zsh + p10k with FiraCode font

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -1,5 +1,31 @@
 { config, pkgs, ... }:
 
 {
-    programs.bash.enable = true;
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    initExtra = ''
+      [[ ! -f ${../files/p10k.zsh} ]] || source ${../files/p10k.zsh}
+    '';
+
+    plugins = with pkgs; [
+      {
+        file = "powerlevel10k.zsh-theme";
+        name = "powerlevel10k";
+        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+      }
+      {
+        file = "p10k.zsh";
+        name = "powerlevel10k-config";
+        src = zsh-powerlevel10k;
+      }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    fira-code-nerdfont
+  ];
+
 }
