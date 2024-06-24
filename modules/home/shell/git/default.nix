468bd046ee39df64cd62c4ec42523b3fffd619d1
Florian RICHER <florian.richer@protonmail.com>
2024-06-24 10:34:22 +0200

    Add git lfs

--- modules/home/shell/git/default.nix
+++ modules/home/shell/git/default.nix
@@ -13,6 +13,8 @@ in
   config = mkIf cfg.enable {
     programs.git = {
       enable = true;
+      lfs.enable = true;
+
       userName = "Florian RICHER";
       userEmail = "florian.richer@protonmail.com";
 
