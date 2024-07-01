f0aabeee50a25d6d8b9ff35c88123670139eb956
Florian RICHER <florian.richer@protonmail.com>
2024-07-01 14:08:09 +0200

    nvidia: powerManagement.enable => true

--- modules/system/hardware/nvidia/default.nix
+++ modules/system/hardware/nvidia/default.nix
@@ -25,7 +25,11 @@ in
         modesetting.enable = true;
 
         # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-        powerManagement.enable = false;
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
+        powerManagement.enable = true;
+
         # Fine-grained power management. Turns off GPU when not in use.
         # Experimental and only works on modern Nvidia GPUs (Turing or newer).
         powerManagement.finegrained = false;
