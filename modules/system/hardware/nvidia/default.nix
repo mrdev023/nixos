be0e89431ae68051c8ad038adbe36b37ee8f152d
Florian RICHER <florian.richer@protonmail.com>
2024-07-01 13:33:47 +0200

    Update OS

--- modules/system/hardware/nvidia/default.nix
+++ modules/system/hardware/nvidia/default.nix
@@ -44,7 +44,7 @@ in
         nvidiaSettings = true;
 
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
       };
     };
   };
