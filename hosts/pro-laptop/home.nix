350f85904bb7d5ba008369cf2ed94f8fd752f571
Florian RICHER <florian.richer@protonmail.com>
2024-09-05 15:08:25 +0200

    pro-laptop: Add firefox

--- hosts/pro-laptop/home.nix
+++ hosts/pro-laptop/home.nix
@@ -8,6 +8,7 @@
   modules.home = {
     apps = {
       chromium.enable = true;
+      firefox.enable = true;
       flatpak.enable = true;
       jetbrainsToolbox.enable = true;
       kitty.enable = true;
