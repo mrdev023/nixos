d08eb3a10779ab11b2f46a44df558a1755ed1016
Florian RICHER <florian.richer@protonmail.com>
2024-09-07 20:55:40 +0200

    tips(kdedev): Add .kde_bashrc

--- tips/distrobox/kde/configure.sh
+++ tips/distrobox/kde/configure.sh
@@ -7,6 +7,13 @@ if [[ $PATH != *".local/bin"* ]]; then
   source ~/.bashrc
 fi
 
+if [[ $PATH != *"alias new_shell"* ]]; then
+  echo "Append new_shell alias to PATH in ~/.bashrc"
+  echo 'alias new_shell="env -u PATH -u QML2_IMPORT_PATH -u QT_PLUGIN_PATH -u NIXPKGS_QT6_QML_IMPORT_PATH -u XDG_CONFIG_DIRS bash -l"' >> ~/.bashrc
+  echo "Reload shell"
+  source ~/.bashrc
+fi
+
 echo "Installing kde-builder"
 curl 'https://invent.kde.org/sdk/kde-builder/-/raw/master/scripts/initial_setup.sh?ref_type=heads' > ~/initial_setup.sh
 bash ~/initial_setup.sh && rm ~/initial_setup.sh
@@ -15,6 +22,6 @@ echo "Run initial setup from kde-builder"
 kde-builder --initial-setup
 
 echo "Install missing dependencies"
-sudo dnf install qt6-qttools-devel
+sudo dnf install qt6-*-devel
 
 echo "Configuration DONE"
--- tips/distrobox/kde/use_distrobox.md
+++ tips/distrobox/kde/use_distrobox.md
@@ -3,10 +3,12 @@
 ## First configuration
 
 1. `distrobox assemble`
-    > You can add --replace to recreate distrobox container
+   > You can add --replace to recreate distrobox container
 2. `distrobox enter kdedev`
 3. `bash configure.sh`
 4. `kde-builder run solid`
+   > You use NixOS ? It's necessary to run `new_shell` alias before.
+   > The alias unset all nixos env variables with nix store references to avoid conflicts with build or run of KDE Shells/Apps/Tools.
 
 ## Usefull config in ~/.config/kdesrc-buildrc
 
