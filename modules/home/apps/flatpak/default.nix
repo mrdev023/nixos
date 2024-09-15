de7a2753df971cd276baeacb7ec5a6b625d04e8e
Florian RICHER <florian.richer@protonmail.com>
2024-09-15 13:59:16 +0200

    flatpak: Remove not use applications

--- modules/home/apps/flatpak/default.nix
+++ modules/home/apps/flatpak/default.nix
@@ -12,7 +12,6 @@ in
   };
 
   config = mkIf cfg.enable {
-
     services.flatpak = {
       enable = true;
 
@@ -24,18 +23,12 @@ in
         { appId = "net.lutris.Lutris"; origin = "flathub";  }
 
         # Pro
-        { appId = "com.slack.Slack"; origin = "flathub";  }
-        { appId = "com.skype.Client"; origin = "flathub";  }
-        { appId = "org.mozilla.Thunderbird"; origin = "flathub"; }
         { appId = "ch.protonmail.protonmail-bridge"; origin = "flathub";  }
         { appId = "org.kde.neochat"; origin = "flathub"; }
 
         # Loisir
         { appId = "com.spotify.Client"; origin = "flathub"; }
-        { appId = "io.gitlab.news_flash.NewsFlash"; origin = "flathub"; }
         { appId = "org.videolan.VLC"; origin = "flathub"; }
-        { appId = "com.obsproject.Studio"; origin = "flathub"; }
-        { appId = "io.github.achetagames.epic_asset_manager"; origin = "flathub"; }
       ];
     };
   };
