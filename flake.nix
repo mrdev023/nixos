771e79522936ebf4bd1dc4f641fd32ca2ca2189b
Florian RICHER <florian.richer@protonmail.com>
2025-01-25 01:05:37 +0100

    Update OS

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
