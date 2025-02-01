31e3bbe9e92379243a2c20ab44cc1e832c1f9793
Florian RICHER <florian.richer@protonmail.com>
2025-02-01 14:13:45 +0100

    qemu: Add secureboot + tmp2 support

--- modules/system/apps/qemu.nix
+++ modules/system/apps/qemu.nix
@@ -16,7 +16,21 @@ in
     users.groups.libvirtd.members = ["florian"];
     users.users.florian.extraGroups = [ "libvirtd" ];
 
-    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [(pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd];
+        };
+      };
+    };
 
     virtualisation.spiceUSBRedirection.enable = true;
   };
