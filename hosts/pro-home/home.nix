e983572b453400a658b16984dbb3f4614499872c
Florian RICHER <florian.richer@protonmail.com>
2025-07-31 10:13:23 +0200

    pro-home: Fix configuration

--- hosts/pro-home/home.nix
+++ hosts/pro-home/home.nix
@@ -1,5 +1,55 @@
-{ ... }:
+{ config, nixgl, pkgs, ... }:
 
 {
-  home-manager.users.florian.imports = [ ./homes/florian.nix ];
+  imports = [
+    ../../modules/home
+  ];
+
+  home = {
+    username = "florian";
+    homeDirectory = "/home/florian";
+  };
+
+  nixGL = {
+    packages = nixgl.packages;
+    installScripts = ["mesa"];
+  };
+
+  modules.home = {
+    apps = {
+      kitty = {
+        enable = true;
+        package = config.lib.nixGL.wrap pkgs.kitty;
+      };
+    };
+
+    shell = {
+      atuin.enable = true;
+      direnv.enable = true;
+      git.enable = true;
+      lazygit.enable = true;
+      zsh.enable = true;
+    };
+
+    editors.neovim.enable = true;
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      enableZshIntegration = true;
+      enableSshSupport = true;
+      pinentry.package = pkgs.pinentry-qt;
+    };
+
+    yubikey-agent.enable = true;
+  };
+
+  programs.gpg = {
+    enable = true;
+
+    scdaemonSettings = {
+      disable-ccid = true;
+    };
+  };
 }
