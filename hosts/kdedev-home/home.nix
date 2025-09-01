5f93f225373fdd8bcf0e47761c2d8cb7ef75b950
Florian RICHER <florian.richer@protonmail.com>
2025-09-01 18:21:31 +0200

    kdedev-home: Remove duplicated configuration with hm_only profile

--- hosts/kdedev-home/home.nix
+++ hosts/kdedev-home/home.nix
@@ -14,24 +14,4 @@
     profiles = [ "shell" "hm_only" ];
     apps.kitty.package = config.lib.nixGL.wrap pkgs.kitty;
   };
-
-  services = {
-    gpg-agent = {
-      enable = true;
-      enableZshIntegration = true;
-      enableSshSupport = true;
-      pinentry.package = pkgs.pinentry-qt;
-    };
-
-    yubikey-agent.enable = true;
-  };
-
-  programs.gpg = {
-    enable = true;
-
-    # Requires: ccid package installed and pcscd system service started
-    scdaemonSettings = {
-      disable-ccid = true;
-    };
-  };
 }
