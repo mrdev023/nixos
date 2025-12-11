8b9878f51f3e1e4f4cf977bc56070dfbdfce8416
Florian RICHER <florian.richer@protonmail.com>
2025-12-11 16:51:58 +0100

    perso-desktop: Patch linux-firmware to fix issue with AMD GPU

--- modules/system/hardware/graphics/amdgpu.nix
+++ modules/system/hardware/graphics/amdgpu.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 let
@@ -11,6 +16,21 @@ in
     '';
   };
   config = mkIf cfg.enable {
+    warnings = [
+      "hardware.firmware.linux-firmware override to 20251111 to fix amdgpu issues. See https://github.com/NixOS/nixpkgs/issues/466945. Remove the override when fixed."
+    ];
+    hardware.firmware = [
+      (pkgs.linux-firmware.overrideAttrs (old: {
+        version = "20251111";
+        src = pkgs.fetchurl {
+          # https://www.kernel.org/pub/linux/kernel/firmware/
+          url = "https://www.kernel.org/pub/linux/kernel/firmware/linux-firmware-20251111.tar.gz";
+          # > nix-prefetch-url https://www.kernel.org/pub/linux/kernel/firmware/linux-firmware-20251111.tar.gz
+          sha256 = "0rp2ah8drcnl7fh9vbawa8p8c9lhvn1d8zkl48ckj20vba0maz2g";
+        };
+      }))
+    ];
+
     hardware.amdgpu = {
       initrd.enable = true;
       opencl.enable = true;
@@ -19,7 +39,7 @@ in
     # Load amdgpu driver for Xorg and Wayland
     services.xserver = {
       enable = true;
-      videoDrivers = ["amdgpu"];
+      videoDrivers = [ "amdgpu" ];
     };
 
     hardware.graphics = {
