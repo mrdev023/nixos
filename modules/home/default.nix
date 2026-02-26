f632e445d03a945de0d15058dddbbeb9f733e737
Florian RICHER <florian.richer@protonmail.com>
2026-02-26 10:57:50 +0100

    home: Auto-configure userDirs

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   imports = [
@@ -13,5 +13,22 @@
 
   home.stateVersion = "26.05";
   programs.home-manager.enable = true;
-}
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    desktop = "${config.home.homeDirectory}/Bureau";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Téléchargements";
+    music = "${config.home.homeDirectory}/Musique";
+    pictures = "${config.home.homeDirectory}/Images";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Modèles";
+    videos = "${config.home.homeDirectory}/Vidéos";
+
+    extraConfig = {
+      PROJECT = "${config.home.homeDirectory}/Projets";
+    };
+  };
+}
