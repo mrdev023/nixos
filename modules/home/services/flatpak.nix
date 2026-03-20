16434fe7c5026effa37524008211c811edfb0f45
Florian RICHER <florian.richer@protonmail.com>
2026-03-20 13:19:24 +0100

    home(flatpak): Remove packages

--- modules/home/services/flatpak.nix
+++ modules/home/services/flatpak.nix
@@ -1,25 +1,5 @@
 { ... }:
 
 {
-  config.services.flatpak = {
-    update.onActivation = true;
-
-    packages = [
-      # Pro
-      {
-        appId = "ch.protonmail.protonmail-bridge";
-        origin = "flathub";
-      }
-      {
-        appId = "org.kde.neochat";
-        origin = "flathub";
-      }
-
-      # Loisir
-      {
-        appId = "org.videolan.VLC";
-        origin = "flathub";
-      }
-    ];
-  };
+  config.services.flatpak.update.onActivation = true;
 }
