728a1e5ae2b62ea587ebf5cad512f5a47fb7722d
Florian RICHER <florian.richer@protonmail.com>
2026-05-05 20:08:16 +0200

    home(shell): Remove AI assistant (prefer manual enabling by configuration)

--- modules/home/profiles.nix
+++ modules/home/profiles.nix
@@ -28,11 +28,6 @@ in
     # Install all default tools to work only from shell
     (mkIf (elem "shell" cfg.profiles) {
       programs = {
-        # Assistant code
-        claude-code.enable = true;
-        # mistral-vibe.enable = true;
-        opencode.enable = true;
-
         atuin.enable = true;
         difftastic.enable = true;
         direnv.enable = true;
