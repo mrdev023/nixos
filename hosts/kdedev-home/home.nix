5f27122bb200f61147b1567cce7d24411ccc98b3
Florian RICHER <florian.richer@protonmail.com>
2026-05-05 20:08:16 +0200

    home(kdedev-home): Use activation instead for authorized_keys

--- hosts/kdedev-home/home.nix
+++ hosts/kdedev-home/home.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
@@ -12,12 +13,11 @@
   home = {
     username = "florian";
     homeDirectory = "/home/florian";
-    file.".ssh/authorized_keys" = {
-      text = ''
+    activation.authorizedKeys = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      install -m 600 ${pkgs.writeText "authorized_keys" ''
         ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5smB5CIE9qvUecEExV6A1wpzkN96d9RMMmfMejYZvm
-      '';
-      mode = "0600";
-    };
+      ''} ~/.ssh/authorized_keys
+    '';
   };
 
   modules.home = {
